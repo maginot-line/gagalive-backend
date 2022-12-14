f940d86cd7f70fe8d2dbdbea8a1a296c0b1404a1
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-12-14 14:25:32 +0900

    2.4 Concurrent Users

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 
 const httpServer = http.createServer(app);
-const io = new Server(httpServer, { cors: { origin: '*' } });
+const io = new Server(httpServer, { cors: { origin: 'https://127.0.0.1:3000', methods: ['GET', 'POST'] } });
 
 const getRooms = () => {
   const {
@@ -22,8 +22,9 @@ const getRooms = () => {
   return roomList;
 };
 
+const users = new Map();
+
 io.on('connection', (socket) => {
-  socket.emit('room_list', getRooms());
   socket.on('join_room', () => {
     const roomList = getRooms();
     let roomId = '';
@@ -32,25 +33,33 @@ io.on('connection', (socket) => {
       const randomString = Math.random().toString(36).substring(2, 11);
       roomId = getTime + randomString;
       socket.join(roomId);
-      socket.emit('welcome', roomId);
+      socket.emit('welcome', 'create', roomId);
     } else {
       roomId = roomList[0];
       socket.join(roomId);
-      socket.emit('welcome', roomId);
+      socket.emit('welcome', 'join', roomId);
     }
     console.log(`room ${roomId} joined`);
+    users.set(socket.id, roomId);
+    socket.emit('concurrent_users', users.size);
   });
-  socket.on('leave_room', (roomId) => {
+  socket.on('leave_room', (roomId, socketId) => {
     if (roomId) {
       socket.leave(roomId);
       console.log(`room ${roomId} left`);
+      if (users.get(socket.id) !== roomId) {
+        console.log('error');
+      } else {
+        users.delete(socket.id);
+        socket.emit('concurrent_users', users.size);
+      }
     }
-    socket.to(roomId).emit('break_room');
+    socket.to(roomId).emit('break_room', socketId);
   });
   socket.on('send_message', (roomId, socketId, msg) => {
     socket.to(roomId).emit('receive_message', socketId, msg);
   });
 });
 
-const handleListen = () => console.log(`Listening on http://localhost:8000`);
+const handleListen = () => console.log(`Listening on http://127.0.0.1:8000`);
 httpServer.listen(8000, handleListen);
